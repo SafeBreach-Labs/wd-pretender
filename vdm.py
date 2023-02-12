59b9e99ce5b688dd45a29c0a58ecd4d948b71a3a
Omer Attias <omer.attias@safebreach.com>
2023-02-12 17:51:21 +0200

    feat: vdm class can now open vdm file and patch the version number

---
+++ vdm.py
@@ -0,0 +1,74 @@
+import pefile
+import struct
+import shutil
+import binascii
+
+class VDM:
+    def __init__(self, path):
+        self.path = path
+        self.pe = pefile.PE(self.path)
+
+    @property
+    def version(self):
+        # Get the version info resource
+        version_info = self.pe.FileInfo[0]
+
+        # Get the string table entry for the "FileVersion" key
+        string_table = version_info[0].StringTable[0]
+        
+        return string_table.entries[b"FileVersion"]
+
+    @version.setter
+    def version(self, new_version):
+        # Get the version info resource
+        version_info = self.pe.FileInfo[0]
+        vs_fixedfileinfo = self.pe.VS_FIXEDFILEINFO[0]
+
+        # Get the string table entry for the "FileVersion" key
+        string_table = version_info[0].StringTable[0]
+        string_table.entries[b'FileVersion']    = new_version
+        string_table.entries[b'ProductVersion'] = new_version
+
+        ms, ls = self.__convert_bytes_version_to_msls(new_version)
+        vs_fixedfileinfo.FileVersionMS = ms
+        vs_fixedfileinfo.FileVersionLS = ls
+    
+    def do_inc_version_build_number(self):
+        cur_version = self.version.split(b'.')
+        
+        # convert the build number to int and inc by 1
+        build_number = int(cur_version[2])
+        build_number = str(build_number + 1).encode()
+
+        cur_version[2]  = build_number
+        new_version     = b'.'.join(cur_version)
+
+        self.version = new_version
+
+    def save(self):
+        self.pe.write(self.path + '.patched')
+        self.pe.close()
+        
+        shutil.move(self.path + '.patched', self.path)
+
+        # reopen vdm file
+        self.pe = pefile.PE(self.path)
+
+    def __convert_bytes_version_to_msls(self, version):
+        version_list = version.split(b'.')
+        version_list = list(map(lambda x: int(x), version_list))
+
+        ms = int(binascii.hexlify((struct.pack('>2H', *version_list[:2]))), base=16)
+        ls = int(binascii.hexlify((struct.pack('>2H', *version_list[2:]))), base=16)
+
+        return ms, ls
+
+def main():
+    vdm = VDM(r"C:\Users\omeratt\work\random\mpasdlta.vdm")
+    print(vdm.version)
+    vdm.do_inc_version_build_number()
+    print(vdm.version)
+
+
+if __name__ == "__main__":
+    main()
