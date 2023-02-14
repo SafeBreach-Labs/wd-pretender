f6fb197f0d11dcb29c6bf793acccd5964ce74e50
lilnand <omer.attias@outlook.com>
2023-02-14 17:01:24 +0200

    feat: added rmdx header structures

--- rmdx.py
+++ rmdx.py
@@ -1,6 +1,34 @@
 import io
-import struct
+import ctypes
+
+class RmdxStruct(ctypes.Structure):
+    _fields_ = [
+        ("Signature", ctypes.c_uint32),
+        ("Timestamp", ctypes.c_uint32),
+        ("Unknown1", ctypes.c_uint32),
+        ("Options", ctypes.c_uint32),
+        ("Unknown2", ctypes.c_uint32),
+        ("Unknown3", ctypes.c_uint32),
+        ("CompressedDataOffset", ctypes.c_uint32),
+        ("DecompressedDataSize", ctypes.c_uint32)
+    ]
+
+class CompressedDataHeaderStruct(ctypes.Structure):
+    _fields_ = [
+        ("CompressedSize", ctypes.c_uint32),
+        ("CompressedCrc", ctypes.c_uint32),
+    ]
 
 class RMDX:
-    def __init__(self):
-        pass
+    def __init__(self, rmdx_data: io.BytesIO):
+        header = rmdx_data.read(0x20)
+        self.rmdx_header = ctypes.cast(header, ctypes.POINTER(RmdxStruct))
+        compressed_offset = self.rmdx_header.contents.CompressedDataOffset
+        
+        # irrelevant data
+        rmdx_data.read(compressed_offset - 0x20) 
+        cdata_header = rmdx_data.read(8)
+
+        self.compressed_data_header = ctypes.cast(cdata_header, ctypes.POINTER(CompressedDataHeaderStruct))
+        self.compressed_data = rmdx_data.read(self.compressed_data_header.contents.CompressedSize)
+        
--- vdm.py
+++ vdm.py
@@ -4,8 +4,10 @@ import struct
 import shutil
 import binascii
 
+from rmdx import *
+
 class VDM:
-    def __init__(self, path):
+    def __init__(self, path: str):
         self.path = path
         self.pe = pefile.PE(self.path)
 
@@ -21,7 +23,7 @@ class VDM:
         return string_table.entries[b"FileVersion"]
 
     @version.setter
-    def version(self, new_version):
+    def version(self, new_version: bytes):
         # Get the version info resource
         version_info = self.pe.FileInfo[0]
         vs_fixedfileinfo = self.pe.VS_FIXEDFILEINFO[0]
@@ -59,17 +61,21 @@ class VDM:
         # reopen vdm file
         self.pe = pefile.PE(self.path)
 
-    def __extract_rmdx_from_resources(self) -> io.BytesIO:
+    def __extract_rmdx_from_resources(self):
+        """
+            looks for RT_RCDATA resource type with resource entry which has the id 1000
+        """
         for resource_type in self.pe.DIRECTORY_ENTRY_RESOURCE.entries:
             if resource_type.name is not None and resource_type.name.__str__() == "RT_RCDATA":
-                
                 for resource_id in resource_type.directory.entries:
                     if resource_id.struct.Id == 1000:
                         data_rva = resource_id.directory.entries[0].data.struct.OffsetToData
                         size = resource_id.directory.entries[0].data.struct.Size
-                        self.rmdx_data = self.pe.get_memory_mapped_image()[data_rva:data_rva+size]                
 
-    def __convert_bytes_version_to_msls(self, version):
+                        data = self.pe.get_memory_mapped_image()[data_rva:data_rva+size]                
+                        self.rmdx_data = RMDX(io.BytesIO(data))
+
+    def __convert_bytes_version_to_msls(self, version: bytes):
         version_list = version.split(b'.')
         version_list = list(map(lambda x: int(x), version_list))
 
@@ -81,7 +87,8 @@ class VDM:
 def main():
     vdm = VDM(r"C:\Users\omeratt\work\random\mpasdlta.vdm")
     rmdx = vdm.get_rmdx()
-    print(hex(len(rmdx)))
+    print(hex(rmdx.compressed_data_header.contents.CompressedSize))
+    print(binascii.hexlify(rmdx.compressed_data[:4]))
 
 
 if __name__ == "__main__":
